c9e3395c6c0746259e03e8d826c1709a4c226a1d
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-04-23 14:58:09 -0700

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+require "pry"
+class Backer
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def back_project(project)
+        ProjectBacker.new(project,self)
+    end
+
+    def backed_projects
+        ProjectBacker.all.select{|project_backer| project_backer.backer == self}.map{|project_backer| project_backer.project}
+    end
+
+end
